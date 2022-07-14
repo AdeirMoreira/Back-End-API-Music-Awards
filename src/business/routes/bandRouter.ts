c85b6041807a5cc6583a266d9bfc6801e68267d4
Eric Felipe <ericfelipess07@gmail.com>
2022-07-14 10:42:27 -0300

    template inicial

---
+++ src/business/routes/bandRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+export const bandRouter = Router()
+
+//Injeção de dependências
+
+bandRouter.post("/create", )
+bandRouter.get("/details", )
---
+++ src/business/routes/showRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+export const showRouter = Router()
+
+//Injeção de dependências
+
+showRouter.post("/register", )
+showRouter.get("/all", )
